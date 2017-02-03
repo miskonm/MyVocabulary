4704c530a504e844f2e1725dda6ab7da84ce7e47
miskonm <miskonm@gmail.com>
2017-02-03 13:33:27 +0300

    Initial Commit

---
+++ MyVocabulary/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyVocabulary
+//
+//  Created by Nikita Misko on 03.02.17.
+//  Copyright © 2017 Nikita Misko. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
